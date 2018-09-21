a1f7515b1a5cb24d943ecab99844301aa1ddaced
smli123 <lishimin@gmail.com>
2018-09-21 11:55:49 +0800

    TR4

--- SmartPlugUDPAPP_GrowLight/src/com/thingzdo/ui/control/DetailGrowLightTimeCurveActivity.java
+++ SmartPlugUDPAPP_GrowLight/src/com/thingzdo/ui/control/DetailGrowLightTimeCurveActivity.java
@@ -1,14 +1,29 @@
 package com.thingzdo.ui.control;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.LineChart;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.CategorySeries;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.thingzdo.dataprovider.SmartPlugHelper;
 import com.thingzdo.internet.RevCmdFromSocketThread;
@@ -24,12 +39,25 @@ public class DetailGrowLightTimeCurveActivity extends TitledActivity
 		implements
 			OnClickListener {
 
+	private LinearLayout ll_chart;
+	private LineChart mLineChart;
+
 	private SmartPlugHelper mPlugHelper = null;
 	private SmartPlugDefine mPlug = null;
 	private String mPlugId = "0";
 	private String mPlugIp = "0.0.0.0";
 
 	private RevCmdFromSocketThread mTcpSocketThread = null;
+
+	private List<Integer> lists = new ArrayList<Integer>();
+
+	private void setLists() {
+		lists.clear();
+		for (int i = 1; i < 20; i++) {
+			int value = ((int) (Math.random() * 100));
+			lists.add(value);
+		}
+	}
 
 	private BroadcastReceiver mDetailRev = new BroadcastReceiver() {
 		@Override
@@ -57,7 +85,8 @@ public class DetailGrowLightTimeCurveActivity extends TitledActivity
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState, R.layout.activity_plug_detail, false);
+		super.onCreate(savedInstanceState,
+				R.layout.activity_plug_detail_growlight_timercurve, false);
 		SmartPlugApplication.resetTask();
 		SmartPlugApplication.getInstance().addActivity(this);
 
@@ -83,6 +112,122 @@ public class DetailGrowLightTimeCurveActivity extends TitledActivity
 			mTcpSocketThread = new RevCmdFromSocketThread();
 			mTcpSocketThread.start();
 		}
+
+		drawTheChart();
+	}
+
+	public void drawTheChart() {
+		XYMultipleSeriesRenderer mRenderer = getXYMulSeriesRenderer();
+
+		XYSeriesRenderer renderer = getXYSeriesRenderer();
+
+		mRenderer.addSeriesRenderer(renderer);
+
+		setLists();
+
+		XYMultipleSeriesDataset dataset = getDataSet();
+
+		GraphicalView chartView = ChartFactory.getLineChartView(this, dataset,
+				mRenderer);
+
+		ll_chart.addView(chartView, 0);
+	}
+
+	public XYSeriesRenderer getXYSeriesRenderer() {
+		XYSeriesRenderer renderer = new XYSeriesRenderer();
+		// 设置折线宽度
+		renderer.setLineWidth(2);
+		// 设置折线颜色
+		renderer.setColor(Color.GRAY);
+		renderer.setDisplayBoundingPoints(true);
+		// 点的样式
+		renderer.setPointStyle(PointStyle.CIRCLE);
+		// 设置点的大小
+		renderer.setPointStrokeWidth(3);
+		// 设置数值显示的字体大小
+		renderer.setChartValuesTextSize(30);
+		// 显示数值
+		renderer.setDisplayChartValues(true);
+		return renderer;
+	}
+
+	public XYMultipleSeriesDataset getDataSet() {
+		XYMultipleSeriesDataset barDataset = new XYMultipleSeriesDataset();
+		CategorySeries barSeries = new CategorySeries("2016年");
+
+		for (int i = 0; i < lists.size(); i++) {
+			barSeries.add(lists.get(i));
+		}
+
+		barDataset.addSeries(barSeries.toXYSeries());
+		return barDataset;
+	}
+
+	public XYMultipleSeriesRenderer getXYMulSeriesRenderer() {
+		XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+		renderer.setMarginsColor(Color.argb(0x00, 0xF3, 0xF3, 0xF3));
+
+		// 设置背景颜色
+		renderer.setApplyBackgroundColor(true);
+		renderer.setBackgroundColor(Color.WHITE);
+
+		// 设置Title的内容和大小
+		renderer.setChartTitle("访问量统计");
+		renderer.setChartTitleTextSize(50);
+
+		// 图表与四周的边距
+		renderer.setMargins(new int[]{80, 80, 50, 50});
+
+		// 设置X,Y轴title的内容和大小
+		renderer.setXTitle("日期");
+		renderer.setYTitle("访问数");
+		renderer.setAxisTitleTextSize(30);
+		// renderer.setAxesColor(Color.WHITE);
+		renderer.setLabelsColor(Color.BLACK);
+
+		// 图例文字的大小
+		renderer.setLegendTextSize(20);
+
+		// x、y轴上刻度颜色和大小
+		renderer.setXLabelsColor(Color.BLACK);
+		renderer.setYLabelsColor(0, Color.BLACK);
+		renderer.setLabelsTextSize(20);
+		renderer.setYLabelsPadding(30);
+
+		// 设置X轴的最小数字和最大数字，由于我们的数据是从1开始，所以设置为0.5就可以在1之前让出一部分
+		// 有兴趣的童鞋可以删除下面两行代码看一下效果
+		renderer.setPanEnabled(false, false);
+
+		// 显示网格
+		renderer.setShowGrid(true);
+
+		// X,Y轴上的数字数量
+		renderer.setXLabels(10);
+		renderer.setYLabels(10);
+
+		// 设置X轴的最小数字和最大数字
+		renderer.setXAxisMin(1);
+		renderer.setXAxisMax(20);
+		// 设置Y轴的最小数字和最大数字
+		renderer.setYAxisMin(0);
+		renderer.setYAxisMax(100);
+
+		// 设置渲染器显示缩放按钮
+		renderer.setZoomButtonsVisible(true);
+		// 设置渲染器允许放大缩小
+		renderer.setZoomEnabled(true);
+		// 消除锯齿
+		renderer.setAntialiasing(true);
+
+		// 刻度线与X轴坐标文字左侧对齐
+		renderer.setXLabelsAlign(Paint.Align.LEFT);
+		// Y轴与Y轴坐标文字左对齐
+		renderer.setYLabelsAlign(Paint.Align.LEFT);
+
+		// 允许左右拖动,但不允许上下拖动.
+		renderer.setPanEnabled(true, false);
+
+		return renderer;
 	}
 
 	@Override
@@ -149,6 +294,9 @@ public class DetailGrowLightTimeCurveActivity extends TitledActivity
 				.getString(R.string.smartplug_growlight_timecurve));
 		setTitleLeftButton(R.string.smartplug_goback,
 				R.drawable.title_btn_selector, this);
+
+		ll_chart = (LinearLayout) findViewById(R.id.ll_chart);
+		// mTf = Typeface.createFromAsset(getAssets(), "OpenSans-Bold.ttf");
 	}
 
 	private void disconnectSocket() {
